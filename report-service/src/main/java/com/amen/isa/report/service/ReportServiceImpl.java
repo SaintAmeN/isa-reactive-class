9fd556220bbc91befa24f02bd442caca5bcd2bec
Paweł Recław <pawel.reclaw@gmail.com>
2023-12-18 14:14:39 +0100

    Zadania: checkpoint 4

--- report-service/src/main/java/com/amen/isa/report/service/ReportServiceImpl.java
+++ report-service/src/main/java/com/amen/isa/report/service/ReportServiceImpl.java
@@ -34,26 +34,61 @@ public class ReportServiceImpl implements IReportService {
         return null;
     }
 
-    @Override
+    //    @Override
+//    public Mono<Report> generateReport(@NonNull LocalDateTime from,
+//                                       @NonNull LocalDateTime to) {
+//        if (!from.isBefore(to)) {
+//            throw new IllegalArgumentException("IAE");
+//        }
+//
+//        Map<Long, Product> products = new ConcurrentHashMap<>();
+//
+//        AtomicLong sales = new AtomicLong(0);
+//        AtomicLong cost = new AtomicLong(0);
+//
+//        return orderServiceClient.fetchOrdersBetweenDates(from, to)
+//                .map(storeOrder -> sumSales(storeOrder, sales))
+//                .then(shipmentServiceClient
+//                              .fetchShipmentsBetweenDates(from, to)
+//                              .map(shipmentResponse -> getFromMap(shipmentResponse, products))
+//                              .map(productMono -> productMono.mapNotNull(product -> sumCost(product, cost)))
+//                              .then())
+//                .then(Mono.just(Report.builder().totalCost(cost.get()).totalSales(sales.get()).build()));
+//    }
+//    public Mono<Report> generateReport(@NonNull LocalDateTime from,
+//                                       @NonNull LocalDateTime to) {
+//        if (!from.isBefore(to)) {
+//            throw new IllegalArgumentException("IAE");
+//        }
+//
+//        return shipmentServiceClient.fetchShipmentsBetweenDates(from, to)
+//                .flatMap(shipmentResponse -> productServiceClient.getProductById(shipmentResponse.getProductId()))
+//                .map(Product::getQuantity)
+//                .reduce(Double::sum)
+//                .flatMap(cost -> {
+//                    return orderServiceClient.fetchOrdersBetweenDates(from, to)
+//                            .map(this::sumSales)
+//                            .reduce(Double::sum)
+//                            .map(sumSales -> Report.builder().totalCost(cost).totalSales(sumSales).build());
+//                });
+//    }
+
     public Mono<Report> generateReport(@NonNull LocalDateTime from,
                                        @NonNull LocalDateTime to) {
         if (!from.isBefore(to)) {
             throw new IllegalArgumentException("IAE");
         }
 
-        Map<Long, Product> products = new ConcurrentHashMap<>();
-
-        AtomicLong sales = new AtomicLong(0);
-        AtomicLong cost = new AtomicLong(0);
-
-        return orderServiceClient.fetchOrdersBetweenDates(from, to)
-                .map(storeOrder -> sumSales(storeOrder, sales))
-                .then(shipmentServiceClient
-                              .fetchShipmentsBetweenDates(from, to)
-                              .map(shipmentResponse -> getFromMap(shipmentResponse, products))
-                              .map(productMono -> productMono.mapNotNull(product -> sumCost(product, cost)))
-                              .then())
-                .then(Mono.just(Report.builder().totalCost(cost.get()).totalSales(sales.get()).build()));
+        return shipmentServiceClient.fetchShipmentsBetweenDates(from, to)
+                .flatMap(shipmentResponse -> productServiceClient.getProductById(shipmentResponse.getProductId()))
+                .map(Product::getQuantity)
+                .reduce(Double::sum)
+                .flatMap(cost -> {
+                    return orderServiceClient.fetchOrdersBetweenDates(from, to)
+                            .map(this::sumSales)
+                            .reduce(Double::sum)
+                            .map(sumSales -> Report.builder().totalCost(cost).totalSales(sumSales).build());
+                });
     }
 
     private Mono<Product> getFromMap(ShipmentResponse shipment, Map<Long, Product> memory) {
@@ -66,6 +101,14 @@ public class ReportServiceImpl implements IReportService {
 
     private ShipmentResponse sumCost(Product product, AtomicLong cost) {
         return null;
+    }
+
+    private Double sumSales(StoreOrder storeOrder) {
+        return storeOrder
+                .getItems()
+                .stream()
+                .mapToDouble(value -> value.quantity().totalPrice() * value.quantity().quantity())
+                .sum();
     }
 
     private StoreOrder sumSales(StoreOrder storeOrder, AtomicLong sales) {
